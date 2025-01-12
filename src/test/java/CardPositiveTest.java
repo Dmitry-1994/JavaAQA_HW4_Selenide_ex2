e1a8a99341822030544f8e3f0f143a5dfd7d8006
Dmitry Tarasov <vizalyyy@gmail.com>
2025-01-13 03:54:01 +0500

    Work on mistakes

--- src/test/java/CardPositiveTest.java
+++ src/test/java/CardPositiveTest.java
@@ -15,34 +15,52 @@ public class CardPositiveTest {
         return LocalDate.now().plusDays(countDay).format(DateTimeFormatter.ofPattern(format));
     }
 
-    private String getDataNow(String format) {
-        return LocalDate.now().format(DateTimeFormatter.ofPattern(format));
+    private int getMonth(int countDay) {
+        return LocalDate.now().plusDays(countDay).getMonthValue();
+    }
+
+    private int getYear(int countDay) {
+        return LocalDate.now().plusDays(countDay).getYear();
+    }
+
+    private void setMonth(int differenceOfMonth) {
+        while (differenceOfMonth > 0) {
+            $(".calendar__arrow[data-step='1']").click();
+            differenceOfMonth--;
+            }
+        while (differenceOfMonth < 0) {
+            $(".calendar__arrow[data-step='-1']").click();
+            differenceOfMonth++;
+            }
+    }
+
+    private void setYear(int differenceOfYear) {
+        while (differenceOfYear > 0) {
+            $(".calendar__arrow[data-step='12']").click();
+            differenceOfYear--;
+        }
     }
 
     @ParameterizedTest
     @CsvSource({
             "Ек, Екатеринбург, 7, Дмитрий, +79122518775",
             "мо, Москва, 28, Дмитрий Тарасов, +12345678910",
-            "на, Ростов-на-Дону, 365, Дмитрий-Тарасов Алексеевич, +99999999999"
+            "на, Ростов-на-Дону, 365, Дмитрий-Тарасов Алексеевич, +99999999999",
+            "на, Ростов-на-Дону, 415, Дмитрий-Тарасов Алексеевич, +99999999999",
+            "на, Ростов-на-Дону, 750, Дмитрий-Тарасов Алексеевич, +99999999999"
     })
-    void shouldRegisterAccount(String cityShort, String cityFull, int correctData, String name, String phone) {
+    void shouldRegisterAccountWithUniversalDate(String cityShort, String cityFull, int correctData, String name, String phone) {
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue(cityShort);
         $$(".menu-item__control").findBy(Condition.exactText(cityFull)).shouldBe(visible).click();
-        $("[data-test-id=date] .icon").click();
 
+        $("[data-test-id=date] .icon").click();
         String setData = getData(correctData, "d");
-        if (getDataNow("yyyy").equals(getData(correctData, "yyyy"))) {
-            if (getDataNow("MM").equals(getData(correctData, "MM"))) {
-                $$(".calendar__day").findBy(Condition.exactText(setData)).shouldBe(visible).click();
-            } else {
-                $(".calendar__arrow[data-step='1']").click();
-                $$(".calendar__day").findBy(Condition.exactText(setData)).shouldBe(visible).click();
-            }
-        } else {
-            $(".calendar__arrow[data-step='12']").click();
-            $$(".calendar__day").findBy(Condition.exactText(setData)).shouldBe(visible).click();
-        }
+        int differenceOfMonth = getMonth(correctData) - getMonth(0);
+        int differenceOfYear = getYear(correctData) - getYear(0);
+        setYear(differenceOfYear);
+        setMonth(differenceOfMonth);
+        $$(".calendar__day").findBy(Condition.exactText(setData)).shouldBe(visible).click();
 
         $("[data-test-id=name] input").setValue(name);
         $("[data-test-id=phone] input").setValue(phone);
